e781567941728f93565dc21ff3685688ab0f6f89
Bogdanov Ilya <onesuch07@gmail.com>
2017-11-07 09:23:48 +0300

    изменил структуру

--- game.js
+++ game.js
@@ -10,7 +10,6 @@ window.onload = function() {
     blade.height = window.innerHeight;
 
     blade.fillStyle  = '#000000';
-    blade.opacity = 0.5;
     blade.fillRect(0, 0, canvas.width, canvas.height);
 
     document.body.appendChild(canvas);
@@ -21,18 +20,23 @@ function colorChange(data) {
     var count = 0,
         arrColors = ['red', 'blue', 'orange', 'green', 'yellow'];
 
-    data.width = window.innerWidth - 5;
-    data.height = window.innerHeight - 5;
+    data.width = window.innerWidth / 2;
+    data.height = window.innerHeight / 2;
     console.log('Вошли во 2-ую функцию', data);
     setInterval(function () {
-        var randomColors = Math.floor(Math.random() * (arrColors.length));
+        var randomColors = Math.floor(Math.random() * (arrColors.length)),
+            randomJumpWidth = Math.floor(Math.random() * window.innerWidth),
+            randomJumpHeight = Math.floor(Math.random() * window.innerWidth);
 
+        data.beginPath();
         data.fillStyle = arrColors[randomColors];
-        console.log('Изменили цвет', data);
+        data.arc(randomJumpWidth, randomJumpHeight, randomColors, 0, 2*Math.PI, false);
+        data.opacity = 0. + randomColors;
+        console.log(data);
         data.fill();
-        data.fillRect(count, count, 100, 100);
+        data.stroke();
         count += 10;
-    }, 500);
+    }, 5);
 }
 
 
